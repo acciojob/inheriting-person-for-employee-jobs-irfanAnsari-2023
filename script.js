9b92215b62580b6500bf6f8c27bf9aa42a296931
Irfan Ansari <123730480+irfanAnsari-2023@users.noreply.github.com>
2025-02-27 21:01:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,30 +1,24 @@
-// complete this js code
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
 
-class Person{
-
-	constructor(name, age){
-		this.name = name;
-		this.age = age;
-	}
-
-	greet(){
-		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
-	}
+    greet() {
+        console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
+    }
 }
 
-class Employee extends Person{
-	constructor(name, age, jobTitle){
-		// this.name = name;
-		// this.age = age;
-		super(name,age)
-		this.jobTitle = jobTitle;
-	}
+class Employee extends Person {
+    constructor(name, age, jobTitle) {
+        super(name, age);
+        this.jobTitle = jobTitle;
+    }
 
-	jobGreet(){
-		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`)
-	}
+    jobGreet() {
+        console.log(`Hello, my name is ${this.name} and I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+    }
 }
-
 
 
 
